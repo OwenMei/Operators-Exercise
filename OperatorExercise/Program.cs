f34d76add050206f13a933f56c51efc1162f3d09
owen mei <owenzackmei@gmail.com>
2023-06-19 11:27:16 -0400

    init commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 14;
+            int b = 9;
+            Console.WriteLine($"{a} + {b} is " + (a + b));
+            Console.WriteLine($"{a} - {b} is " + (a - b));
+            Console.WriteLine($"{a} * {b} is " + (a * b));
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} with a remainder of {remainder}");
+            Console.WriteLine("\n\n What is the radius of your circle?");
+            var input = Console.ReadLine();
+            int radius = (input!=null) ?  int.Parse(input) : 1;
+            Console.WriteLine("The area of the circle is "+AreaOfCircle(radius));
+        }
+        public static double AreaOfCircle(int radius)
+        {
+            return(Math.PI * radius);
         }
     }
 }
